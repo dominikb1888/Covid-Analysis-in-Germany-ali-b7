584dfda4a7d1ab6d3f124395cddc058ab202b1bb
Ali Badran <115889052+ali-b7@users.noreply.github.com>
2023-11-22 17:50:58 +0100

    Update mainproject.py
    fixes to the slider, trouble having it interactively appear on map

--- mainproject.py
+++ mainproject.py
@@ -2,6 +2,8 @@ import pandas as pd
 import geopandas as gpd
 import folium
 from folium.plugins import MarkerCluster, HeatMap
+import ipywidgets as widgets
+from ipywidgets import interactive
 
 # Load your GeoJSON data
 geojson_path = 'bundeslande.geojson'
@@ -18,32 +20,6 @@ data = pd.read_csv(csv_path)
 # Merge GeoJSON data with CSV data on the common column
 merged_data = gdf.merge(data, left_on='GEN', right_on='Bundesland')
 
-# Function to update GeoJSON layer based on the filter
-def update_map(cases_threshold):
-    filtered_data = merged_data[merged_data['Covid Cases'] >= cases_threshold]
-    
-    # Remove previous Choropleth layer
-    for layer in m._children.values():
-        if isinstance(layer, folium.Choropleth):
-            m.remove_layer(layer)
-    
-    # Add a new Choropleth layer with the filtered data
-    folium.Choropleth(geo_data=filtered_data,
-                      name='Filtered Covid Data',
-                      data=filtered_data,
-                      columns=['Bundesland', 'Covid Cases'],
-                      key_on='feature.properties.Bundesland',
-                      fill_color='YlOrRd',
-                      fill_opacity=0.7,
-                      line_opacity=0.2,
-                      legend_name='Covid Cases',
-                      highlight=True,
-                     ).add_to(m)
-
-# Create an interactive slider for filtering COVID cases
-cases_slider = widgets.FloatSlider(value=0, min=0, max=500000, step=10000, description='Min COVID Cases:')
-interactive(update_map, cases_threshold=cases_slider)
-
 
 # Create a folium map centered around the mean latitude and longitude of the GeoJSON data
 map_center = [merged_data.geometry.centroid.y.mean(), merged_data.geometry.centroid.x.mean()]
@@ -59,6 +35,38 @@ folium.GeoJson(merged_data,
                                              aliases=['Region', 'Covid Cases'],
                                              localize=True),
                ).add_to(marker_cluster)
+
+
+# Function to update GeoJSON layer based on the filter
+# Function to update GeoJSON layer based on the filter
+def update_map(cases_threshold):
+    filtered_data = merged_data[merged_data['Covid Cases'] >= cases_threshold]
+    
+    # Remove previous Choropleth layer
+    for layer in m._children.values():
+        if isinstance(layer, folium.Choropleth):
+            m.remove_layer(layer)
+    
+    # Add a new Choropleth layer with the filtered data
+    folium.Choropleth(geo_data=filtered_data,
+                      name='Filtered Covid Data',
+                      data=filtered_data,
+                      columns=['Bundesland', 'Total Deaths'],
+                      key_on='feature.properties.Bundesland',
+                      fill_color='YlOrRd',
+                      fill_opacity=0.7,
+                      line_opacity=0.2,
+                      legend_name='Deaths',
+                      highlight=True,
+                     ).add_to(m)
+
+# Create an interactive slider for filtering COVID cases
+cases_slider = widgets.FloatSlider(value=0, min=0, max=500000, step=10000, description='Min COVID Cases:')
+widget = interactive(update_map, cases_threshold=cases_slider)
+widget.children[-1].layout.height = 'auto'  # Adjusting the height for better visibility
+display(widget)
+
+
 # adding data as markers
 for idx, row in merged_data.iterrows():
     # Choose marker color based on Covid Cases
@@ -73,9 +81,11 @@ for idx, row in merged_data.iterrows():
                   popup=f"<strong>{row['GEN']}</strong><br>Population: {row['Population']}<br>Vaccination Rate: {row['Vaccination Rate']}<br>Covid Cases: {row['Covid Cases']}<br>Total Deaths: {row['Total Deaths']}",
                   icon=folium.Icon(color=marker_color),
                   ).add_to(marker_cluster)
+    
+    
   # Add Legend 
     legend_html = """
-     <div style="position: fixed; 
+        <div style="position: fixed; 
                  bottom: 50px; left: 50px; width: 160px; height: 150px; 
                  border:2px solid grey; z-index:9999; font-size:12px;
                  background-color: white;
@@ -87,11 +97,6 @@ for idx, row in merged_data.iterrows():
       </div>
      """
 
-# Include Font Awesome CSS in the head
-font_awesome_link = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">'
-m.get_root().html.header.add_child(folium.Element(font_awesome_link))     
-
-# add legend
 m.get_root().html.add_child(folium.Element(legend_html))
 
 # Create a HeatMap layer using the location coordinates and intensity (e.g., COVID cases)
@@ -102,4 +107,4 @@ HeatMap(heat_data, name='Heatmap', radius=25, blur=20, gradient={0.4: '#FFD700',
 folium.LayerControl().add_to(m)
 
 # Save the map as an HTML file
-m.save('de_covidmapfinal.html')
+m.save('de_covidmapfinal_filtered.html')
