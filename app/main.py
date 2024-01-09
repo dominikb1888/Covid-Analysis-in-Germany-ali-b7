58a50cf608cb01ffabc154ba20f11992e6fdc90e
Ali Badran <115889052+ali-b7@users.noreply.github.com>
2024-01-09 14:05:38 +0000

    copied to app folder

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI()
+
+# Serve static files (HTML, JS, CSS)
+app.mount("/", StaticFiles(directory="./", html=True), name="static")
+
+
+# Endpoint to serve the HTML file
+@app.get("/")
+def get_html():
+    return FileResponse("./index.html")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Run the FastAPI app with uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+
+#uvicorn main:app --host 127.0.0.1 --port 8000 --reload
