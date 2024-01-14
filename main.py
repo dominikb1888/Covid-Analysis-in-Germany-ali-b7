7fc316688dd6e59502398837803a6e510dc277b6
Ali Badran <115889052+ali-b7@users.noreply.github.com>
2024-01-14 11:18:29 +0000

    devcontainer created and working properly

--- main.py
+++ main.py
@@ -21,4 +21,5 @@ if __name__ == "__main__":
     # Run the FastAPI app with uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
 
+# run application
 # uvicorn main:app --host 127.0.0.1 --port 8000 --reload
