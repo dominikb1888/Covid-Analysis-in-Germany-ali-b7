4819349b79cd3b443c07c7096eb3ebbe8f8cc63d
Ali Badran <115889052+ali-b7@users.noreply.github.com>
2024-01-15 17:59:06 +0000

    database connected successfully

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def read_items():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
 
 # run application
 # uvicorn main:app --host 127.0.0.1 --port 8080 --reload
